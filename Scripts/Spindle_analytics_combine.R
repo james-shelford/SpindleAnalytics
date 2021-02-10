de1c5ea32f9c2991d4ef3540011e9495b53fb776
James Shelford <j.shelford@warwick.ac.uk>
2021-02-10 12:55:01 +0000

    update script

--- Scripts/Spindle_analytics_combine.R
+++ Scripts/Spindle_analytics_combine.R
@@ -42,17 +42,17 @@ combined_df$Condition <- factor(combined_df$Condition, levels = c('Condition 1',
 combined_df$Treatment <- factor(combined_df$Treatment, levels = c('Control', 'Treatment'))
 
 # Function to generate the plots
-
+# theme_cowplot removes grey background
 Make_The_Plot <- function(dataframe_to_plot, measurement_to_plot, y_label){
-  the_plot <- ggplot(data = dataframe_to_plot, aes_string(x = 'Condition', y = measurement_to_plot, colour = 'Treatment')) +
-    geom_quasirandom(alpha = 0.5, stroke=0, dodge.width = 1) + 
-    stat_summary(fun.data = mean_se, geom = 'point', size = 2, aes(group = Treatment), position = position_dodge(1)) +
-    stat_summary(fun.data = mean_sdl, fun.args = list(mult=1), geom = 'errorbar', size = 0.8, aes(group = Treatment), position = position_dodge(1), width = 0) +
+  the_plot <- ggplot(data = dataframe_to_plot, aes_string(x = 'Condition', y = measurement_to_plot, fill = 'Treatment')) +
+    theme_cowplot() +
+    geom_violin(position = position_dodge(0.9)) +
+    geom_boxplot(width=0.2, outlier.shape = NA, position = position_dodge(0.9), show.legend = FALSE) +
     theme(axis.text.x = element_text(face = "plain", color = 'black', size = 8, angle = 0, hjust = 0.5, vjust = 0.5), axis.text.y = element_text(face = 'plain', color = 'black', size = 8)) +
     theme(axis.title.y = element_text(size = 9,face = 'plain', color = 'black')) +
     labs(y = y_label, x = NULL) + 
     theme(legend.title = element_blank()) + 
-    theme(legend.position = 'none') +
+    theme(legend.position = "bottom") +
     ylim(0, NA)
   return(the_plot)
 }
@@ -60,25 +60,34 @@ Make_The_Plot <- function(dataframe_to_plot, measurement_to_plot, y_label){
 # Make the plots
 spindle_length <- Make_The_Plot(combined_df, 'centrosome_centrosome_dist', 'Spindle length (microns)')
 spindle_width <- Make_The_Plot(combined_df, "spindle_width", "Spindle width (microns)")
-spindle_aspect_ratio <- Make_The_Plot(combined_df, "spindle_aspect_ratio", "Spindle aspect ratio (length/width)")
+spindle_aspect_ratio <- Make_The_Plot(combined_df, "spindle_aspect_ratio", "Spindle aspect ratio")
 spindle_angle <- Make_The_Plot(combined_df, "spindle_angle", "Spindle angle (degrees)")
-
 d1_d2_shift <- Make_The_Plot(combined_df, "d1_d2_offset", "Spindle shift d2-d1 (microns)")
 l1_l2_shift <- Make_The_Plot(combined_df, "l1_l2_offset", "Spindle shift l2-l1 (microns)")
 D1_D2_shift <- Make_The_Plot(combined_df, "D1_D2_offset", "Spindle shift D2-D1 (microns)")
 offset_distance <- Make_The_Plot(combined_df, "offset_dist", "Spindle offset distance (microns)")
 
 # combine plots
-spindle_lengths_plot <- plot_grid(spindle_length, spindle_aspect_ratio)
 # labels = 'AUTO' generates uppercase labels
 # align = 'hv' aligns the plots horizontally and vertically
-spindle_shifts_plot <- plot_grid(d1_d2_shift, D1_D2_shift, l1_l2_shift, offset_distance, labels = 'AUTO', align = 'hv', rel_widths = c(1, 1), rel_heights = c(1,1)) + theme(aspect.ratio=1)
+combined_plot <- plot_grid(spindle_length + theme(legend.position = "none"), spindle_width + theme(legend.position = "none"), 
+                           spindle_aspect_ratio + theme(legend.position = "none"), spindle_angle + theme(legend.position = "none"),
+                           d1_d2_shift + theme(legend.position = "none"), D1_D2_shift + theme(legend.position = "none"),
+                                 l1_l2_shift + theme(legend.position = "none"), offset_distance + theme(legend.position = "none"),
+                                 labels = 'AUTO', align = 'hv', rel_widths = c(1, 1), rel_heights = c(1,1)) + theme(aspect.ratio=1)
+combined_plot
+
+# Extract a legend from one of the plots to add back
+legend <- get_legend(d1_d2_shift + guides(color = guide_legend(nrow = 1)) +
+                       theme(legend.position = "bottom"))
+
+# Add the legend to the bottom of the combined plot
+combined_plot <- plot_grid(combined_plot, legend, ncol = 1, rel_heights = c(1, .05))
+combined_plot
 
 # Save the plots
-ggsave("Output/Plots/combined_spindle_length_plot.png", plot = spindle_lengths_plot, dpi = 300)
-ggsave("Output/Plots/combined_spindle_length_plot.pdf", plot = spindle_lengths_plot, width = 250, height = 250, units = 'mm', useDingbats = FALSE)
-ggsave("Output/Plots/combined_spindle_shift_plot.png", plot = spindle_shifts_plot, dpi = 300)
-ggsave("Output/Plots/combined_spindle_shift_plot.pdf", plot = spindle_shifts_plot, width = 250, height = 250, units = 'mm', useDingbats = FALSE)
+ggsave("Output/Plots/combined_plot.png", plot = combined_plot, dpi = 300)
+ggsave("Output/Plots/combined_plot.pdf", plot = combined_plot, width = 250, height = 250, units = 'mm', useDingbats = FALSE)
 
 # Summary statistics
 summary_table <- combined_df %>% group_by(Condition, Treatment) %>% summarise_at(vars(centrosome_centrosome_dist), list(mean_spindle_length = mean, standard_dev = sd))
